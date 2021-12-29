9e518f81d2615b42543f4a8a93758d62073b367e
atomheartdani <contact@danielemeregalli.com>
2021-12-29 15:45:37 +0100

    Decouple secret key

--- django_dashboard/django_dashboard/settings.py
+++ django_dashboard/django_dashboard/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 import os
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h2%cdy4$ik_r822!z=#!&jcxw=j&r9097(!dpg&^$&zyjxirwx'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
